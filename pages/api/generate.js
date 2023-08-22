7597f3ea8d48cdf18f9b2752482c5279c5ef04f6
ihadula3 <ihadula@gmail.com>
2023-08-21 22:19:41 -0400

    updated for gpt-4

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,27 +1,37 @@
-import { Configuration, OpenAIApi } from "openai";
+//import { Configuration, OpenAIApi } from "openai";
+import OpenAI from 'openai';
 
-const configuration = new Configuration({
+/*const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);*/
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
 
 export default async function (req, res) {
-  const completion = await openai.createCompletion("text-davinci-001", {
-    prompt: generatePrompt(req.body.animal),
+  const completion = await openai.chat.completions.create({
+  	model: "gpt-4",
+    messages: generateConversation(req.body.animal),
     temperature: 0.6,
   });
-  res.status(200).json({ result: completion.data.choices[0].text });
+  res.status(200).json({ result: completion.choices[0].message.content });
+
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
+function generateConversation(animal) {
+  var capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  var messages = [];
+  messages.push({"role": "system", "content": "Suggest three names for an animal that is a superhero."});
+  messages.push({"role": "user", "content": "Cat"});
+  messages.push({"role": "assistant", "content": "Captain Sharpclaw, Agent Fluffball, The Incredible Feline"});
+  messages.push({"role": "user", "content": "Dog"});
+  messages.push({"role": "assistant", "content": "Ruff the Protector, Wonder Canine, Sir Barks-a-Lot"});
+  messages.push({"role": "user", "content": `${capitalizedAnimal}`});
+  
+  //console.log(messages);
+  return messages;
 }
